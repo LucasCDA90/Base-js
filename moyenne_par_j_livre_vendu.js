5fb43030fee00cc80f956a43b5cdced1e294942c
LucasCDA90 <lucas.brg7@gmail.com>
2024-04-09 09:30:36 +0200

    test

--- moyenne_par_j_livre_vendu.js
+++ moyenne_par_j_livre_vendu.js
@@ -1,5 +1,5 @@
 let total_livre_vendu = 650                 //Initialisation du nombre de livres vendus
 total_livre_vendu += 50                     //Ajout de 50 livres
 moyenne_par_jour = total_livre_vendu/7      //Calcul de la moyenne
-let message = "Le nombre de livres vendus par jour est :"
-console.log(message,moyenne_par_jour)
+let message = "Le nombre de livres vendus par jour est : test"            //Message
+console.log(message,moyenne_par_jour)       //Affichage
