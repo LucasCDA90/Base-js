d76db274100186c608f29ffa5386ed534ec4d41e
LucasCDA90 <lucas.brg7@gmail.com>
2024-04-09 11:55:03 +0200

    Ajout de branch

--- produit_object.js
+++ produit_object.js
@@ -4,8 +4,18 @@ let produit = {
     stock: 20,
     nb_vendu: 10
 }
-console.log(produit)
-produit.gain = produit.prix*produit.nb_vendu
-console.log(produit)
-produit.valeur_stock = produit.stock*produit.prix
-console.log("Produit :", produit.nom, " / Stock :", produit.valeur_stock, " / Gain :", produit.gain)
+console.log(produit)                                                                                                //Affichage des données
+produit.gain = produit.prix*produit.nb_vendu                                                                        //Création du gain       
+console.log(produit)                                                                                                //Affichage des caracteristiques du produit
+produit.valeur_stock = produit.stock*produit.prix                                                                   //Création de la valeur du stock
+console.log("Produit :", produit.nom, " / Stock :", produit.valeur_stock, " / Gain :", produit.gain)                //Affichage des données
+
+produit.cost = 1                                                                                                    //Initialisation du cout d'une pomme
+produit.total_cost = produit.cost*(produit.nb_vendu + produit.stock)                                                //Création de l'achat de produit
+console.log("Somme d'achat des produits :", produit.total_cost)                                                     //Affichage du cout total
+
+let benefice_par_produit = produit.prix - produit.cost                                                              //Création du bénéfice par produit
+console.log("Bénéfice par produit :", benefice_par_produit)                                                         //Calcul du bénéfice
+
+let pourcentage_benefice = 100*(benefice_par_produit/produit.prix)                                                  //Création du pourcentgae de bénéfice
+console.log("Pourcentage bénéfice par produit", pourcentage_benefice+"% ~", Math.round(pourcentage_benefice)+"%")   //Affichage du pourcentage ainsi que l'arrondi du pourcentage
