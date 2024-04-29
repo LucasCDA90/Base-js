2d386264e391006d9d8790b0cc1c99c41ed66aae
LucasCDA90 <lucas.brg7@gmail.com>
2024-04-29 16:29:58 +0200

    Exo

--- events.js
+++ events.js
@@ -21,3 +21,21 @@ setTimeout(function() {
     }
 
 }, 3000)
+
+function waiting(time) {
+    return new Promise ((resolve, reject) => {
+        if (time > 100)
+        setTimeout(()=> {
+            console.log("OK")
+            resolve({succes : true})
+        }, time)
+        else 
+            reject({succes : false})
+        })
+}
+
+waiting(101).then((value) => {
+    console.log("Promise réussie :", value)
+}).catch((err) => {
+    console.log("Promise échoué :", err)
+})
